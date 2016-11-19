da8be53dcec058a8f2338ea7045b0faec9073d14
YuzuruS <navitima@gmail.com>
2016-11-19 16:30:33 +0900

    fix travis_mecab

--- .travis_mecab.sh
+++ .travis_mecab.sh
@@ -3,4 +3,4 @@ set -ex
 wget https://github.com/rsky/php-mecab/archive/master.zip
 unzip master.zip
 cd php-mecab-master/mecab && phpize && ./configure --with-php-config=/usr/bin/php-config --with-mecab-config=/usr/bin/mecab-config && make && sudo make install
-echo "extension=<extension>.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension=mecab.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
