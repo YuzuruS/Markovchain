fe5a7a9ca42a543cc19bdb1ed7d655c1d4fb3381
YuzuruS <navitima@gmail.com>
2016-11-19 14:38:25 +0900

    fix test

--- tests/MarkovchainTest.php
+++ tests/MarkovchainTest.php
@@ -12,25 +12,7 @@ use YuzuruS\Mecab\Markovchain;
 class MarkovChainTest extends \PHPUnit_Framework_TestCase
 {
 	private $markov;
-	/**
-	 * Provider for rating
-	 *
-	 * @return array
-	 */
-	public function textProvider()
-	{
-		return [
-			[
-				'繁栄を築き上げた人類は、突如出現した“天敵”「巨人」により滅亡の淵に立たされた。生き残った人類は、「ウォール・マリア」、「ウォール・ローゼ」、「ウォール・シーナ」という巨大な三重の城壁の内側に生活圏を確保することで、辛うじてその命脈を保っていた。城壁による平和を得てから約100年後。いつしか人類は巨人の脅威を忘れ、平和な日々の生活に埋没していた。',
-			],
-			[
-				'ほげ',
-			],
-			[
-				'',
-			]
-		];
-	}
+
 	/**
 	 * setUp
 	 *
@@ -42,32 +24,24 @@ class MarkovChainTest extends \PHPUnit_Framework_TestCase
 		$this->markov = new MarkovChain();
 	}
 
-	/**
-	 * @dataProvider textProvider
-	 */
-	public function testMakeMarkovText1($text)
+	public function testMakeMarkovText1()
 	{
-		$return = $this->markov->makeMarkovText($text);
+		$samples = [
+			'繁栄を築き上げた人類は、突如出現した“天敵”「巨人」により滅亡の淵に立たされた。生き残った人類は、「ウォール・マリア」、「ウォール・ローゼ」、「ウォール・シーナ」という巨大な三重の城壁の内側に生活圏を確保することで、辛うじてその命脈を保っていた。城壁による平和を得てから約100年後。いつしか人類は巨人の脅威を忘れ、平和な日々の生活に埋没していた。',
+			'ほげ',
+			'',
+		];
+
+		$return = $this->markov->makeMarkovText($samples[0]);
 		$this->assertTrue(!empty($return));
-	}
 
-	/**
-	 * @dataProvider textProvider
-	 */
-	public function testMakeMarkovText2($text)
-	{
-		$return = $this->markov->makeMarkovText($text);
+		$return = $this->markov->makeMarkovText($samples[1]);
+		$this->assertFalse($return);
+
+		$return = $this->markov->makeMarkovText($samples[2]);
 		$this->assertFalse($return);
 	}
 
-	/**
-	 * @dataProvider textProvider
-	 */
-	public function testMakeMarkovText3($text)
-	{
-		$return = $this->markov->makeMarkovText($text);
-		$this->assertFalse($return);
-	}
 
 	public function tearDown()
 	{
